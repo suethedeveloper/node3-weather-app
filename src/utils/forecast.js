d0c199f4dbc576615838d318e0a46019e4bc1e16
Sue Lucas <choi.soyoung@gmail.com>
2019-11-30 15:51:39 -0800

    adding high & low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require("request");
+console.log("XXXXX")
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/d45ba786571a346f69a6bfb4cf7604ef/${longitude},${latitude}`;
     request({url, json: true}, (err, {body})=> {
@@ -8,8 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location.", undefined);
         } else {
             const {currently} = body;
-            // const currently = response.body.currently;
-            const msg = `${body.daily.data[0].summary} It is currently ${currently.temperature} degrees out. There is a ${currently.precipProbability * 100}% chance of rain.`;
+            const msg = `${body.daily.data[0].summary} It is currently ${currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh} with a low of  ${body.daily.data[0].temperatureLow}. There is a ${currently.precipProbability * 100}% chance of rain.`;
             callback(undefined, msg);
             // 
         }
